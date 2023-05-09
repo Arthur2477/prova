e69d662bd3d70c94a8bbadb4ac118e5b2dd5a817
Arthur2477 <arthurrodrigues3080@gmail.com>
2023-05-08 21:56:00 -0300

    1

---
+++ componen/Pg.js
@@ -0,0 +1,25 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react'
+import Cb from './Cabecalho'
+import Rd from './Rodape'
+import { Container } from 'react-bootstrap';
+
+const Pagina = (props) => {
+
+    return (
+        <>
+            <Cb />
+            <div className='bg-secondary text-white py-3 text-center mb-3'>
+                <h1>{props.titulo}</h1>
+            </div>
+            
+            <Container className='mb-5'>
+                {props.children}
+            </Container>
+
+            <Rd />
+        </>
+    )
+}
+
+export default Pagina
